5c6d36c1f7723e57526ea8d2ee2f9134b4d049f6
shaishi <shai.shilo@gmail.com>
2017-06-20 02:12:11 +0300

    Added geolocation

---
+++ g8/Views/HitchhikerPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace g8.Views
+{
+    public partial class HitchhikerPage : ContentPage
+    {
+        public HitchhikerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
